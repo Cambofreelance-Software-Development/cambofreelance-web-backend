dfb95aa9be051f0f0911532736d69a72b5236647
dinsarendev <dinsarendev@gmail.com>
2026-06-19 13:19:19 +0700

    feat(flyway): Migration update

--- src/main/java/com/cambofreelance/webbackend/configs/BeanConfig.java
+++ src/main/java/com/cambofreelance/webbackend/configs/BeanConfig.java
@@ -1,5 +1,6 @@
 package com.cambofreelance.webbackend.configs;
 
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -9,5 +10,13 @@ public class BeanConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> {
+            flyway.baseline();
+            flyway.migrate();
+        };
     }
 }
